897477c5aeca2003a535d0b0653cd34faae1eb1f
chung yang <chung.yang@engineer.uconn.edu>
2018-07-04 11:20:57 +0800

    Use shortcut function to make views more concise

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse, Http404
 from .models import Question
@@ -6,18 +6,13 @@ from django.template import loader
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
-    context = {
-        'latest_question_list': latest_question_list,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
-    return render(request, 'polls/detail.html', {'question': question})
+    question = get_object_or_404(Question, pk = question_id)
+    context = {'question' : question}
+    return render(request, 'polls/details.html',context)
 
 def results(request, question_id):
     response = "You're looking at the results of question %s."
